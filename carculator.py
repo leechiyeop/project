17418f5488d7d245d38df3adc5b1bc96b8b1e42a
LAPTOP-YEOP\chiyeop <leechiyeop2003@gmail.com>
2018-03-17 21:53:02 +0900

    랜덤으로 문제생성 함수

--- carculator.py
+++ carculator.py
@@ -1,1 +1,15 @@
-print("Hello world!")
+import random as r
+
+def making_questions() :
+	a = r.randint(1,32)
+	b = r.randint(1,3)
+	c = r.randint(4,93)
+	
+	if b == 1 : buho = '+'
+	elif b == 2 : buho = '-'
+	else : buho = '*'
+	
+	ques = str(a)+' '+buho+' '+str(c)+' = '
+	
+	return ques
+
