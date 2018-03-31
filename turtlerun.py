f389e910f4502e62d89efe23be58c8b89d489bd8
LAPTOP-YEOP\chiyeop <leechiyeop2003@gmail.com>
2018-03-31 21:33:43 +0900

    주석달기 및 정리

--- turtlerun.py
+++ turtlerun.py
@@ -1,8 +1,10 @@
+#모듈가져오기
 import turtle as t
 import random as r
 devil = t.Turtle()
 feed = t.Turtle()
 
+#함수생성
 def turnup() :
 	t.setheading(90)
 def turndown() :
@@ -12,16 +14,19 @@ def turnright() :
 def turnleft() :
 	t.setheading(180)
 
+#악당
 devil.shape('turtle')
 devil.up()
 devil.speed(0)
 devil.color('red')
 devil.goto(r.randint(-229, 229), r.randint(-229, 229))
 
+#먹이
 feed.shape('circle')
 feed.up()
 feed.goto(r.randint(-229, 229), r.randint(-229, 229))
 
+#주인공
 t.setup(500,500)
 t.shape('turtle')
 t.color('white')
@@ -34,5 +39,6 @@ t.onkeypress(turnleft, 'Left')
 t.onkeypress(turnright, 'Right')
 t.goto(0,0)
 
+#마무리
 t.listen()
 t.mainloop()
