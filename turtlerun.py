3aa1ec742ea788fdcbb13a3d3ecc096da35bdf0f
LAPTOP-YEOP\chiyeop <leechiyeop2003@gmail.com>
2018-03-31 21:08:11 +0900

    화면구성

--- turtlerun.py
+++ turtlerun.py
@@ -1,1 +1,22 @@
+import turtle as t
 
+t.setup(500,500)
+t.shape('turtle')
+t.color('white')
+t.bgcolor('gray')
+t.speed(0)
+t.up()
+t.onkeypress(up,'Up')
+t.onkeypress(down, 'Down')
+t.onkeypress(left, 'Left')
+t.onkeypress(right, 'Right')
+
+def up() :
+	
+def down() :
+	
+def right() :
+	
+def left() :
+	
+t.mainloop()
